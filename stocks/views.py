5c41e84fca7e916e05b2d1242a7441a53659769b
Rupesh Mangal <rupesh.code@gmail.com>
2017-12-12 14:11:11 +0530

    adding dates

--- stocks/views.py
+++ stocks/views.py
@@ -13,16 +13,33 @@ def index(request):
         return render(request,'stocks/homepage.html')
     elif request.method=="POST":
         symbol = request.POST['symbol']
-        print(symbol)
-        return redirect(reverse('graph_view') + '?u=%s' % symbol)
+        start_date = request.POST['start_date']
+        end_date = request.POST['end_date']
+
+#        print(symbol)
+        data_list = []
+        data_list.append(symbol)
+        data_list.append(start_date)
+        data_list.append(end_date)
+
+        return graph_view(request, data_list)
+#        return redirect(reverse('graph_view') + '?smb=%s' % symbol + '?sd=%s' % start_date + '?ed=%s' % end_date)
 
     else:
         pass
 
 
-def graph_view(request):
-    symbol = request.GET.get('u', None)
-    df = stockdata.get_stock_data(symbol)
+def graph_view(request,data_list):
+#    symbol = request.GET.get('smb', None)
+    symbol = data_list[0]
+    start_date = data_list[1]
+    end_date = data_list[2]
+#    start_date = request.GET.get('sd', None)
+#    end_date = request.GET.get('ed', None)
+#    print(start_date)
+#    print(end_date)
+#    print(symbol)
+    df = stockdata.get_stock_data(symbol, start_date, end_date)
     plot = plotstockdata.plot_graph(df,symbol)
     script, div = components(plot)
     #print(script,div)
